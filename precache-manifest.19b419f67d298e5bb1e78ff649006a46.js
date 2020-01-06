365e4803844f276c8d595b796f6f2ce6ae9d4fe0
Ruben Jacob <uhgul@student.kit.edu>
2020-01-06 17:06:46 +0100

    Updates

---
+++ precache-manifest.19b419f67d298e5bb1e78ff649006a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910505f2c6e406e58d953266ae7c32bd",
+    "url": "/MCWebsite/index.html"
+  },
+  {
+    "revision": "ac2f454e8b40d7b13357",
+    "url": "/MCWebsite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5cffc360f1f1fe64555a",
+    "url": "/MCWebsite/static/js/2.9317f329.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MCWebsite/static/js/2.9317f329.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac2f454e8b40d7b13357",
+    "url": "/MCWebsite/static/js/main.36f68862.chunk.js"
+  },
+  {
+    "revision": "cfd6b884f6820a52618b",
+    "url": "/MCWebsite/static/js/runtime-main.4f995de1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MCWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
